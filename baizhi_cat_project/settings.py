e32c00aa3ab15a1a677c8b6ada2f611eca13efef
“DeerKing007 <1005304721@qq.com>
2019-02-20 21:28:55 +0800

    登录注册没毛病

--- baizhi_cat_project/settings.py
+++ baizhi_cat_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ifsl1gers99s0rc78lwc+)d2eyx2%a9h$cp7tbs9asn=0olx8='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 

0256cbdb63bf4fa969769089344e9076cfb41eca
fyc <747061804@qq.com>
2019-02-20 17:44:34 +0800

    详情页的数据传输

--- baizhi_cat_project/settings.py
+++ baizhi_cat_project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'menu_app',
 ]
 
 MIDDLEWARE = [
@@ -76,8 +77,12 @@ WSGI_APPLICATION = 'baizhi_cat_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ssss',
+        'PASSWORD': '123456',
+        'PORT': '3306',
+        'HOST': 'localhost',
+        'USER':'root',
     }
 }
 
@@ -119,3 +124,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'static')]

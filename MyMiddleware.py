f11033c34c68f2328a24721619c56123307cd52e
lzhjust <1306704804@qq.com>
2019-02-21 14:29:00 +0800

    refer 反爬虫

--- MyMiddleware.py
+++ MyMiddleware.py
@@ -10,7 +10,10 @@ class MyMiddleAware(MiddlewareMixin):  # 自定义的中间件
     # view处理请求前执行
     def process_request(self, request):
         user = request.META.get('HTTP_USER_AGENT')
-        print('user-agent', user)
+        ip = request.META.get('REMOTE_ADDR')
+        ip1= request.META.get('HTTP_X_FORWARDED_FOR')
+        # print('user-agent', user)
+        # print('ip', ip,'####',ip1)
         if 'Windows' in user or 'Mac' in user:
             pass
             # print("request:", request)
--- menu_app/views.py
+++ menu_app/views.py
@@ -9,31 +9,39 @@ CITYID=['','北京','上海','广州','深圳']
 def menu_view(request):
     return render(request,'menu.html')
 
+
 def menu_show_data(request):
-    position_id=request.GET.get('position_id')
-    city_id=request.GET.get('city_id')
-    page=request.GET.get('page')
-    if not city_id:
-        city_id=1
+    refer=request.META.get('HTTP_REFERER')
+    print('##########')
+    print(refer)
+    if refer=="http://127.0.0.1:8000/main_app/main/":
+        position_id=request.GET.get('position_id')
+        city_id=request.GET.get('city_id')
+        page=request.GET.get('page')
+        if not city_id:
+            city_id=1
+        else:
+            city_id=int(city_id)
+        if not page:
+            page=1
+        else:
+            page=int(page)
+        data=Job.objects.filter(job_name__contains=position_id).filter(company_addr__contains=CITYID[city_id]).values()
+        pagtor = Paginator(data, per_page=10)
+        data = pagtor.page(page)
+        page_count=divmod(pagtor.count,10)[0]
+        if divmod(pagtor.count,10)[1] !=0:
+            page_count+=1
+        data_count=pagtor.count
+        return render(request,'menu.html',
+                      {"data":data,
+                       'position_id':position_id,
+                       'city_id':city_id,
+                       "page_count":page_count,
+                       "data_count":data_count,
+                       "page":page,
+                       })
+
     else:
-        city_id=int(city_id)
-    if not page:
-        page=1
-    else:
-        page=int(page)
-    data=Job.objects.filter(job_name__contains=position_id).filter(company_addr__contains=CITYID[city_id]).values()
-    pagtor = Paginator(data, per_page=10)
-    data = pagtor.page(page)
-    page_count=divmod(pagtor.count,10)[0]
-    if divmod(pagtor.count,10)[1] !=0:
-        page_count+=1
-    data_count=pagtor.count
-    return render(request,'menu.html',
-                  {"data":data,
-                   'position_id':position_id,
-                   'city_id':city_id,
-                   "page_count":page_count,
-                   "data_count":data_count,
-                   "page":page,
-                   })
+        return render(request, '404.html')
 

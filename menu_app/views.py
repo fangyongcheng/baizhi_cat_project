6f37e4dad90665dac3b7400bf8b28ba0e17cf5e4
“DeerKing007 <1005304721@qq.com>
2019-02-21 20:53:27 +0800

    F12

--- menu_app/views.py
+++ menu_app/views.py
@@ -27,13 +27,11 @@ def menu_show_data(request):
             city_id = int(city_id)
         data = Job.objects.filter(job_name__contains=position_id).filter(
             company_addr__contains=CITYID[city_id]).values()
-    print(data.count())
     if not page:
         page = 1
     else:
         page = int(page)
     pagtor = Paginator(data, per_page=10)
-    print('############')
     try:
         data = pagtor.page(page)
     except:

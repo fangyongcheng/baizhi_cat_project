e4258e24bf9c50cc6b44b39dc18d139880d8aa39
fyc <747061804@qq.com>
2019-02-21 17:21:19 +0800

    修改了反爬虫中间介

--- baizhi_cat_project/againstClawlerMiddleware.py
+++ baizhi_cat_project/againstClawlerMiddleware.py
@@ -10,14 +10,16 @@ class MyMiddleAware(MiddlewareMixin):  # 自定义的中间件
         self.time_count=time.time()+60
     # view处理请求前执行
     def process_request(self, request):
-        if request.META["HTTP_HOST"] in self.ips:
-            self.ips[request.META["HTTP_HOST"]]+=1
-            if self.ips[request.META["HTTP_HOST"]]>=60:
+        print(request.META)
+        print(request.META["REMOTE_ADDR"])
+        if request.META["REMOTE_ADDR"] in self.ips:
+            self.ips[request.META["REMOTE_ADDR"]]+=1
+            if self.ips[request.META["REMOTE_ADDR"]]>=60:
                 if self.time_count<time.time():
                     self.time_count = time.time() + 60
-                    self.ips[request.META["HTTP_HOST"]]=0
+                    self.ips[request.META["REMOTE_ADDR"]]=0
                 return render(request,'crawler_error.html')
 
         else:
-            self.ips[request.META["HTTP_HOST"]]=1
+            self.ips[request.META["REMOTE_ADDR"]]=1
 

61486ffc411210472bae9d55f77d1847f555fa34
“DeerKing007 <1005304721@qq.com>
2019-02-21 17:30:10 +0800

    强登，验证码

--- baizhi_cat_project/againstClawlerMiddleware.py
+++ baizhi_cat_project/againstClawlerMiddleware.py
@@ -10,8 +10,6 @@ class MyMiddleAware(MiddlewareMixin):  # 自定义的中间件
         self.time_count=time.time()+60
     # view处理请求前执行
     def process_request(self, request):
-        print(request.META)
-        print(request.META["REMOTE_ADDR"])
         if request.META["REMOTE_ADDR"] in self.ips:
             self.ips[request.META["REMOTE_ADDR"]]+=1
             if self.ips[request.META["REMOTE_ADDR"]]>=60:
